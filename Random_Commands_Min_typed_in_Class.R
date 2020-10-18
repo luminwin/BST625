75de80a38532c2e3bbbd3c695a49ad282ff5956b
Min Lu <luminwin@gmail.com>
2020-10-18 15:49:04 -0400

    Some random commands I mentioned

---
+++ Random_Commands_Min_typed_in_Class.R
@@ -0,0 +1,38 @@
+search() ## get your "global" environments 
+ls() ## list all the objects in your ".GlobalEnv" environment
+head(esoph) ## check your data through your R console 
+tail(esoph) ## check your data through your R console 
+contributors() ## check the Authors of R and core group of R
+mean(c("1", "2")) ## warning sign
+debugonce(mean) ## go inside of R functions
+mean(c("1", "2"))
+crazy.mean <- function (x, trim = 0, na.rm = FALSE, ...) 
+{
+  if (!is.numeric(x) && !is.complex(x) && !is.logical(x)) {
+   # warning("argument is not numeric or logical: returning NA")
+    return("Happy October Fools Day")
+  }
+  if (na.rm) 
+    x <- x[!is.na(x)]
+  if (!is.numeric(trim) || length(trim) != 1L) 
+    stop("'trim' must be numeric of length one")
+  n <- length(x)
+  if (trim > 0 && n) {
+    if (is.complex(x)) 
+      stop("trimmed means are not defined for complex data")
+    if (anyNA(x)) 
+      return(NA_real_)
+    if (trim >= 0.5) 
+      return(stats::median(x, na.rm = FALSE))
+    lo <- floor(n * trim) + 1
+    hi <- n + 1 - lo
+    x <- sort.int(x, partial = unique(c(lo, hi)))[lo:hi]
+  }
+  .Internal(mean(x))
+}
+
+crazy.mean(c("1", "2"))
+
+x <- 5 ## A value is still a vector in R
+x
+x[3] ## x[3] is NA but it is still there
