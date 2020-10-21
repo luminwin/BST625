3fd9404ee4f97423b6cd3938222503eaa50c678f
min <min@imac.local>
2020-10-21 09:08:38 -0400

    tibble

--- R3.1.R
+++ R3.1.R
@@ -1,3 +1,21 @@
-head(mtcars)
 # install.packages("tidyverse")
 library(dplyr) 
+head(mtcars)
+mtcars %>% head
+mtcars %>% head(., )
+
+##################################
+# tibble is similar to data.frame
+##################################
+######################## slide 5
+
+baliseTbl <- tibble(names = c("Raymond", "Lori", "Mary", "Andrew"),  
+                    heightInInches = c(67, 62, 60.01, 68),  
+                    isFemale = c(FALSE, TRUE, TRUE, FALSE))
+
+names <- c("Raymond", "Lori", "Mary", "Andrew")
+heightInInches <- c(67, 62, 60.01, 68)
+isFemale <- c(FALSE, TRUE, TRUE, FALSE)
+baliseNamedList2 <- list(Names = names, "Height in Inches" = heightInInches, "Is Female" = isFemale)
+
+balises <- as_tibble(baliseNamedList2)
