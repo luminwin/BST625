d8153ed7e57d05798791c686ed2c8a225229a984
min <min@imac.local>
2020-10-21 09:15:22 -0400

    glimpse

--- R3.1.R
+++ R3.1.R
@@ -20,5 +20,8 @@ names <- c("Raymond", "Lori", "Mary", "Andrew")
 heightInInches <- c(67, 62, 60.01, 68)
 isFemale <- c(FALSE, TRUE, TRUE, FALSE)
 baliseNamedList2 <- list(Names = names, "Height in Inches" = heightInInches, "Is Female" = isFemale)
-
+as.data.frame(baliseNamedList2)
 balises <- as_tibble(baliseNamedList2)
+######################## slide 6
+glimpse(baliseTbl ) 
+
