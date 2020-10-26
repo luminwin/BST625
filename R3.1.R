b33171db917621d603a00806470fb78332028825
min <min@imac.local>
2020-10-26 09:18:58 -0400

    library

--- R3.1.R
+++ R3.1.R
@@ -124,7 +124,7 @@ laryngectomy %>%
 ######################################################10/26/2020
 laryngectomy <- read_excel("laryngectomy.xls")
 
-library(tidyverse) 
+library(tidyverse)   
 # install.packages("tidyverse")
 laryngectomy %>%
   select(ends_with("_fu"))
