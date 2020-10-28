5b024ef1693ce57db3f55e5c4ecf7476b7fdf2cf
min <min@imac.local>
2020-10-28 08:59:50 -0400

    write_csv

--- R3.2.R
+++ R3.2.R
@@ -1,10 +1,8 @@
 ########################################################
 library(tidyverse)
-library(readr)
 dat <- read_csv("score_data999.csv", 
                 col_types = cols(score1 = col_integer(), 
                                  score2 = col_integer(), score3 = col_integer(), 
                                  gender = col_factor(levels = c("m", "f"))))
 
 dat[dat == 999] <- NA
-write_csv(x = dat, path = "score_data.csv")
