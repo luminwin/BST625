9f38f75db4036485871e7afc508b64e2a3565f35
min <min@imac.local>
2020-11-03 19:49:18 -0500

    123

--- 3-data-read-write.R
+++ 3-data-read-write.R
@@ -3,5 +3,12 @@ dat <- read_csv("~/Dropbox/R book/GitR/BST625/score_data.csv",
                             score2 = col_integer(), score3 = col_integer(), 
                             gender = col_factor(levels = c("m","f"))))
 
+##### dat is a tibble
 write_csv(dat, file = "score_data.csv")
 save(dat, file = "score_data.RData")
+
+
+dat <- read.csv("~/Dropbox/R book/GitR/BST625/score_data.csv")
+
+##### dat is a data.frame
+write.csv(dat, file = "score_data.csv", row.names = FALSE)
