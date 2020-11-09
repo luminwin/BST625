e8537ecd8e840cc95eebacc7c218a7d1cb2c19ae
min <min@imac.local>
2020-11-09 10:39:01 -0500

    filter

--- R5_base_project.R
+++ R5_base_project.R
@@ -1,3 +1,8 @@
 dta.state <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", header = TRUE)
-head(dta.state)
+head(dta.state, 10)
 tail(dta.state)
+
+
+###############################################
+dta.FL <- dta.state[dta.state$state == "Florida",]
+########### Now I want to calculate daily cases and deaths
