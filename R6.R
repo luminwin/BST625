da463816d772ddba81501a0ee73b3eacdebcad67
min <min@imac.local>
2020-11-11 09:57:58 -0500

    123

--- R6.R
+++ R6.R
@@ -102,3 +102,19 @@ dat %>% mutate(
   # Default smoother is LOESS
   geom_smooth() +
   geom_smooth(method = "lm", colour = "red")
+
+########################################### slide # 7
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  ggplot(aes(gender, Avg_Score, fill = factor(major))) +
+  geom_boxplot()     
