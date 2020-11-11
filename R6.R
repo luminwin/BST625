2d3ebc5c968606061d9674089a978987c264d548
min <min@imac.local>
2020-11-11 09:09:02 -0500

    123

--- R6.R
+++ R6.R
@@ -1,2 +1,10 @@
-# !!!!!!!!!!!!! load this data, this data is different from your .csv
+# !!!!!!!!!!!!! load this data, this data is different from your .csv !!!!!
 load("~/Dropbox/R book/R6/score_data.RData")
+
+library(tidyverse)
+?mpg # check the data
+ggplot(data = mpg) +
+  aes(x = displ, y = hwy) + 
+  geom_point(aes(colour = cyl)) + 
+  geom_smooth(method = "lm", colour = "red")
+
