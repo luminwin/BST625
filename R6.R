61743ed3b08996e28231a74a60468d68cf2c8109
min <min@imac.local>
2020-11-11 09:41:31 -0500

    123

--- R6.R
+++ R6.R
@@ -9,7 +9,7 @@ ggplot(data = mpg) +
   # Default smoother is LOESS
   geom_smooth() +
   geom_smooth(method = "lm", colour = "red") +
-  ggsave(height = 4, width = 4*1.9,
+  ggsave(height = 3, width = 3*1.9,
          file = "scatterexample.jpg")
 
 
@@ -29,3 +29,21 @@ dat %>% mutate(
   ggplot() +
   geom_bar(mapping = aes(x = grade)) +
   labs(x = "Grade", y="Count Number")
+
+
+########################################### slide # 5
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  ggplot() +
+  geom_count(mapping = aes(x = grade, y = gender)) +
+  labs(x = "Grade", y="Female/Male")
