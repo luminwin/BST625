e4f4a5d2c9e9422b0625b04254aab16251f51e62
min <min@imac.local>
2020-11-11 09:50:01 -0500

    123

--- R6.R
+++ R6.R
@@ -62,3 +62,20 @@ dat <- dat %>% mutate(
     grade == "F"~"Fail", 
     TRUE ~"Pass"))
 
+########################################### slide # 6
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  count(grade, gender) %>%  
+  ggplot(mapping = aes(x = grade, y = gender)) +
+  geom_tile(mapping = aes(fill = n))+
+  labs(x = "Grade", y="Female/Male")
