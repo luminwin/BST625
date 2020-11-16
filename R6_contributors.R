fd06924f8373465c0d547fe7edbe649475ca3b8a
min <min@imac.local>
2020-11-16 01:00:02 -0500

    123

--- R6_contributors.R
+++ R6_contributors.R
@@ -10,9 +10,3 @@ library(nycflights13)
 library(tidyverse)
 dat <- read_csv("http://courses.washington.edu/b517/Datasets/shoulder.csv")
 flights <- flights
-
-
-datw <- dat %>% 
-  pivot_wider(names_from = time, 
-              values_from = pain, 
-              names_prefix = "pain")
