0575c277480bb04199f14a1d45737ca134c556b8
min <min@imac.local>
2020-11-16 09:39:20 -0500

    123

--- R6_contributors.R
+++ R6_contributors.R
@@ -62,3 +62,30 @@ barplot(Pain_long)
 barplot(table(Pain_long),
         xlab = "Pain level",
         ylab = "Frequency")
+
+#####################################
+# contributor: Min Lu {add-on package}
+#  hist() is for continuous var
+#  our "pain" variable is not continuous enough
+#  Let me fake a continuous pain
+#####################################
+#  install.packages("hrbrthemes")
+library(hrbrthemes)
+
+# Build dataset with different distributions
+datf <- data.frame(
+  time = c( rep("Time 1", 500), rep("Time 2", 500),
+            rep("Time 3", 500), rep("Time 4", 500) ),
+  Pain = c( rnorm(500, mean = 0), rnorm(500, mean=5),
+            rnorm(500, mean = 10), rnorm(500, mean=15))
+)
+
+# Represent it
+datf %>%
+  ggplot( aes(x = Pain, fill = time)) +
+  geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
+  # scale_fill_manual(values=c("#69b3a2", "#404080")) +
+  theme_ipsum() +
+  labs(fill="")
+
+detach("package:hrbrthemes", unload=TRUE)
