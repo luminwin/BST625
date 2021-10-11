fb124ce8cb907b30936cfa2d88127f057a20b1df
Min Lu <luminwin@gmail.com>
2021-10-11 09:52:50 -0400

    d

--- Broadcasting.R
+++ Broadcasting.R
@@ -23,9 +23,18 @@ ls()  # print all the objects created by you
 ## windows users should install Rtools at
 ## https://cran.rstudio.com/bin/windows/Rtools/
 
-#install.packages("metavcov")  # install a package
-#library("metavcov")           # load a package
-#require("metavcov")           # load a package
+install.packages("metavcov")  # install a package
+library("metavcov")           # load a package
+require("metavcov")           # load a package
+
+age <- 5:10
+mean(age) # calculate the average/mean
+mean(x = age, trim = 0.2, na.rm = TRUE) # trimmed mean
+
+mean(c(22, 15, 90, 55))
+
+mean(age, 0.2, TRUE)  # the same as mean(x = age, trim = 0.2, na.rm = TRUE)
+mean(age, 0.2)        # the same as mean(x = age, trim = 0.2, na.rm = FALSE)
 
 
 
