5248f43148ba077cd32a8a271d2b0c1d36ae90b6
Min Lu <luminwin@gmail.com>
2021-10-13 09:34:00 -0400

    se

--- Broadcasting.R
+++ Broadcasting.R
@@ -9,8 +9,21 @@ age <- c(11, 22, 15, 50, 30, 71)
 #mean(age)
 
 IfedisMean <- function(x) {
-  x0 <- x[-c(which.min(x), which.max(x))]
+  index <- c(which.max(x))
+  x0 <- x[-index]
   mean(x0)
 }
 IfedisMean(age)
 mean(age)
+
+## two basic atomic classes
+### double or integer, here numeric vector is a general term to refer to both
+
+## input each element one by one
+age <- c(11, 22, 15, 50, 30, 71) # real or decimal 
+
+age <- c(age, 6, 7, 3, 10) # add more elements
+indices <- 1:10  # integer, colon : generates a sequence in steps of 1 or -1
+x <- rep(0, 5) # repeat 0 5 times
+typeof(age) ## get the type of the vector
+typeof(indices)
