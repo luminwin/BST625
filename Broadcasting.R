6bf4f88d01d48f7297df87152a99880da210b6e8
Min Lu <luminwin@gmail.com>
2021-10-13 09:52:38 -0400

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -63,3 +63,33 @@ height <- 21:30
 mean(height[which(age>8)]) ## calculate the mean for elements that are greater than 8
 
 
+x <- age[7:10]
+##111111111  the following functions only apply to a numeric vector
+range(x) #range
+min(x)   # minimum value/smallest value
+max(x)   # maximum value/largest value
+
+
+sort(x)  # sort the value
+order(x) # Ordering Permutation
+x[order(x)] # the same as sort()
+
+
+abs(x)   # absolute value
+log(x)   # logarithm transform
+sqrt(x)  # square root
+age^2      # squared
+age^3      # to the third power
+
+round(sqrt(x)) # round the result, () for function implementation ordered from inside to outside
+round(sqrt(x), digits = 2) # set digits for round()
+round(sqrt(x), 2) # the same as above
+floor(x) # floor integer
+ceiling(x) # ceiling integer
+
+mean(x)    # mean
+sd(x)      # standard deviation
+var(x)     # variance
+median(x)  # median
+##111111111  end
+
