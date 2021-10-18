2af4f7c4fe3d3756a84db4e7f15287995efc0676
Min Lu <luminwin@gmail.com>
2021-10-18 09:16:12 -0400

    aa

--- Broadcasting.R
+++ Broadcasting.R
@@ -7,3 +7,21 @@ age <- c(11, 22, 15, 50, 30, 71)
 height <- c(140, 160, 156, 170, 180, 175)
 mean(height[which(age>20)])   # conditional mean: average body height for participants older                                                       #  than 20 years old
 height[order(age)]                    #  display body height after sorting age in an increasing order
+
+
+############
+# vector
+############
+
+age <- c(10, 12, 25, 50, 55, 80)
+height <- c(120, 118, 162, 160, 169, 170)
+
+############
+# matrix
+############
+
+cbind(age, height) # create a matrix by binding the columns
+rbind(age, height) # create a matrix by binding the rows
+# create a matrix by specifying each element
+mdat <- matrix(c(1,2,3, 11,12,13), nrow = 3, ncol = 2) 
+mdat
