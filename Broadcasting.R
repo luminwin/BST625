3f21205cfa4d632e93350ecf36b09bab7d041e5d
Min Lu <luminwin@gmail.com>
2021-10-18 09:07:25 -0400

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,3 +3,7 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
+age <- c(11, 22, 15, 50, 30, 71)  
+height <- c(140, 160, 156, 170, 180, 175)
+mean(height[which(age>20)])   # conditional mean: average body height for participants older                                                       #  than 20 years old
+height[order(age)]                    #  display body height after sorting age in an increasing order
