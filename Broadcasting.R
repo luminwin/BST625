ed42e5de9bb84f350d1c9997feee636c6e9a6002
Min Lu <luminwin@gmail.com>
2021-10-20 09:09:39 -0400

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -2,3 +2,11 @@
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
+
+r1 <- c('I', 'am', 'happy')
+r2 <- c('what', 'a', 'day')
+r3 <- c(1,2,3)
+C <- cbind(r1, r2, r3) ## r3 is coerced into character shown with quotation marks
+
+typeof(C)
+typeof(r3)
