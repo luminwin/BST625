a50f8073fd24dce876f4a8a577c39c87f7e434bf
Min Lu <luminwin@gmail.com>
2021-10-25 09:03:26 -0400

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -2,3 +2,7 @@
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
+
+age <- c(11, 22, 15, 50, 30, 71) 
+order(age)
+age[order(age)]
