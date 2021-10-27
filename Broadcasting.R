878e08d374eb2e527369f6dc5c85e02e37b182ee
Min Lu <luminwin@gmail.com>
2021-10-27 08:27:45 -0400

    20211027

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,3 +3,13 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
+names <- c("Min", "Robert", "Mary", "Hemant")
+heightInCM <- c(165, 185, 170, 180)
+isMale <- c(FALSE, TRUE, FALSE, TRUE)
+mydata.frame <- data.frame(Name = names, Height = heightInCM, IsMale = isMale)
+
+
+if("tidyverse" %in% rownames(installed.packages()) == FALSE) {install.packages("tidyverse")}
+library(tidyverse)
+
+myTibble <- as_tibble(mydata.frame)
