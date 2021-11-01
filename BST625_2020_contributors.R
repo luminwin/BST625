2747e5a3fad59516aed80dffa975bbc9ba825df9
Min Lu <luminwin@gmail.com>
2021-11-01 09:50:43 -0400

    ss

--- BST625_2020_contributors.R
+++ BST625_2020_contributors.R
@@ -5,7 +5,6 @@
 #install.packages("nycflights13")
 #install.packages("lubridate")
 
-
 library(nycflights13)
 library(tidyverse)
 dat <- read_csv("http://courses.washington.edu/b517/Datasets/shoulder.csv")
