64e1b12cc2675bc1a71d6bb74a3264eb4c64c989
Min Lu <luminwin@gmail.com>
2021-11-08 08:36:49 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -2,3 +2,12 @@
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
+
+## Go to Blackboard => R => Code for Exercise
+## Download
+## File 1-data-read-write.R 
+## File score_data.RData 
+## File score_data.txt 
+## File score_data.xlsx 
+## File score_data999.csv 
+## File score_data.csv 
