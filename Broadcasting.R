4e7483a47e91105d4fd4dea3f59d33c589fb328d
Min Lu <luminwin@gmail.com>
2021-11-08 09:46:55 -0500

    aa

--- Broadcasting.R
+++ Broadcasting.R
@@ -68,3 +68,31 @@ rm(score_data, score_data999)
 X <- mtcars[1:5, c("mpg", "cyl", "disp")]
 
 t(X)
+
+###################################################################	
+###
+### R code for your SAS project 2
+###
+###################################################################	
+
+##  (Total_Score) Total score as the sum of score 1 to 3
+##  (Avg_Score) Average score as the mean of the score 1 to 3
+##  (grade)         grade A: Avg_Score >= 90
+##                        B: Avg_Score [80, 90)
+##                        C: Avg_Score [70, 80)
+##                        D: Avg_Score [60, 70)
+##                        F: Avg_Score [0, 60)
+##  (pass) grade A to D: "Pass", grade F: "Fail"
+
+##  create a .csv file in your local machine that contains (name, gender and the above 4 new variables)
+##  Data Q1.Score_m contains male students and the students have an average score sorted from low to high.
+##  Data Q1.Score_f contains female students and the students have an average score sorted score from high to low.
+
+
+######################
+# {base}
+######################
+########################## slide 6
+dat <- read.csv("https://luminwin.github.io/BST625/score_data.csv")
+
+dat[1:3,] <- dat[1,]
