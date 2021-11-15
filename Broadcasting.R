3832177829b032d95f50e24d061c519d2d138bac
Min Lu <luminwin@gmail.com>
2021-11-14 23:07:53 -0500

    20211114

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,3 +3,4 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
+############ go to Blackboard and download score_data.RData from R6
