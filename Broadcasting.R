1f8eec82e76fbdc5b1b4fbc3eb53f8cf2b10c65b
Min Lu <luminwin@gmail.com>
2021-11-15 10:10:11 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -197,3 +197,28 @@ dat %>% mutate(
         axis.text = element_text(size = 12),
         axis.title = element_text(size = 14, face = "bold"))
 
+Age <- c(50, 55, 60, 65, 70)                                     # Age groups  
+Male <- c(15.4, 24.3, 37.0, 54.6, 71.1)                          # Death rates for males  
+Female <- c(8.4, 13.6, 19.3, 35.1, 50.0)                         # Death rates for females  
+Deathrate <- matrix(c(Male,Female), nrow = length(Age), ncol = 2, 
+                    dimnames = list(Age, c("Male","Female")))         
+# Create a matrix Deathrate with the data  
+Deathrate2 = t(Deathrate)                                       # Transpose the Deathrate matrix  
+
+# SIMPLE BARPLOT                                        # Open a new window for the plot  
+
+barplot(Deathrate2,                             # Data (bar heights) to plot  
+        beside = TRUE,                            # Plot the bars beside one another; default is to plot stacked bars  
+        space = c(0.2,0.8),                       # Amount of space between i) bars within a group, ii) bars between groups  
+        names.arg = c("65-69", "60-64", "55-59", "50-54", "70-74"),    #Names for the bars  
+        col = c("blue", "red"),                   # Color of the bars  
+        border = "black",                         # Color of the bar borders  
+        main = c("Death rates in Virginia"),      # Main title for the plot  
+        xlab = "Age group",                       # X-axis label  
+        ylab = "Death rate",                      # Y-axis label  
+        font.lab = 2)                             # Font to use for the axis labels: 1=plain text, 2=bold, 3=italic, 4=bold italic  
+
+legend("topleft",                               # Add a legend to the plot  
+       legend=c("Male", "Female"),             # Text for the legend  
+       fill=c("blue", "red"))                  # Fill for boxes of the legend  
+
