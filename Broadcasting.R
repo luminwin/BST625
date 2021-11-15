88b6b184c175151410a1129e2f72e5dbe853521c
Min Lu <luminwin@gmail.com>
2021-11-15 09:45:58 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -111,3 +111,19 @@ dat %>% mutate(
   ggplot() +
   geom_bar(mapping = aes(x = grade), fill = "blue") +
   labs(x = "Grade", y="Count Number")
+
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  ggplot() +
+  geom_count(mapping = aes(x = grade, y = gender)) +
+  labs(x = "Grade", y="Female/Male")
