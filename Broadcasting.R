24973606f3934836283f80917e6f5d782f58c531
Min Lu <luminwin@gmail.com>
2021-11-15 09:38:49 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -72,3 +72,22 @@ dat %>% mutate(
   geom_point(aes(colour = Avg_Score)) + 
   geom_smooth() +
   geom_smooth(method = "lm", colour = "red")
+
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  ggplot() +
+  aes(x = GRE, y = GPA_stats_II) + 
+  geom_point(aes(colour = GPA_stats_I)) + 
+  # Default smoother is LOESS
+  geom_smooth() +
+  geom_smooth(method = "lm", colour = "red")
