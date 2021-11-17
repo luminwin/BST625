217b265686dc9a62cd1aeffb460f7f3d386ccf0a
Min Lu <luminwin@gmail.com>
2021-11-17 09:01:22 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -5,3 +5,33 @@
 
 # download team.jpeg from blackboard R6 and save it in your working directory
 
+###################################################
+#
+#         Homework 6
+#
+###################################################
+
+dat <- read.csv("http://courses.washington.edu/b517/Datasets/shoulder.csv")
+dat <- reshape(dat, v.names = "pain", idvar = "id",
+               timevar = "time", direction = "wide")
+dat$Avg_Pain <- rowMeans(dat[,paste("pain", 1:6, sep = ".")])
+
+par(mfrow = c(1, 2)) 
+boxplot(Avg_Pain ~ trt + sex, data = dat)
+
+## remember that trt == 1 for "Control" and trt == 2 for "Post-operative suction"?
+cols <- c("chocolate1", "chartreuse4")
+dat$sex <- factor(dat$sex, levels = 1:2, labels = c("male", "female"))
+dat$trt <- ifelse(dat$trt == 1, "control", "treat") 
+boxplot(Avg_Pain ~ trt + sex, data = dat, col = cols)
+
+par(mfrow = c(1, 2)) 
+boxplot(Avg_Pain ~ trt + sex, data = dat, col = cols)
+boxplot(Avg_Pain ~ trt + sex, data = dat,
+        at = c(1:2 , 4:5) , col = cols ,
+        names = c("XXXXA", "XXXXB", "XXXXA", "XXXXB"),
+        xaxs = FALSE,
+        xlab = "XXXXC",
+        ylab = "XXXXD")
+legend("topleft", fill = cols ,
+       legend = c("XXXX1", "XXXX2") , horiz = T, bty = "n")
