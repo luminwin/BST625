d090b1e02a18850ebc8010380aa3cc63b06d7652
Min Lu <luminwin@gmail.com>
2021-11-17 09:26:45 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -48,3 +48,40 @@ boxplot(Avg_Pain ~ trt + sex, data = dat,
         ylab = "Average pain level")
 legend("topleft", fill = cols ,
        legend = c("Control", "Treatment") , horiz = T, bty = "n")
+
+############### Question B.1 and T.1
+dat <- read.csv("http://courses.washington.edu/b517/Datasets/shoulder.csv")
+dat <- reshape(dat, v.names = "pain", idvar = "id",
+               timevar = "time", direction = "wide")
+
+
+######### !!!! pay attention to upper case or lower case: Avg_Pain or Avg_pain
+dat$Avg_Pain <- rowMeans(dat[,paste("pain", 1:6, sep = ".")])
+
+library(tidyverse)
+dat <- dat %>% mutate(Avg_Pain = rowMeans(select(.,pain1:pain6)))
+
+################## side 11
+################## R style guide
+
+
+####### the following code has bad style
+
+# comments are retained ; a comment block will be reflowed if it contains long comments;' roxygen comments will not be wrapped in any case
+1+1
+if( TRUE ) {
+  x =1 # inline comments
+} else {
+  x =2; print ('Oh no ... ask the right bracket to go away !') }
+1*3 # one space before this comment will become two!
+2+2+2 # only "single quotes are allowed in comments
+lm(y~x1+x2,data=data.frame(y=rnorm(100),x1=rnorm(100),x2=rnorm(100))) ### a linear model
+
+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1 
+# comment after a long line here is a long long long long long long long long long comment that may be wrapped
+
+
+########### !!!! copy the above commend and run below
+if("formatR" %in% rownames(installed.packages()) == FALSE) {install.packages("formatR")}
+library(formatR)
+tidy_source(width.cutoff = 50) # can set arbitrary width here
