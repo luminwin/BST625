e89a3e273584790955e673e68aa63ff101ea1fea
Min Lu <luminwin@gmail.com>
2021-11-29 08:52:38 -0500

    20211129

--- Broadcasting.R
+++ Broadcasting.R
@@ -2,3 +2,21 @@
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
+
+checkuse <-function(x){
+  
+  if(x %in% rownames(installed.packages()) == FALSE) {
+    
+    install.packages(x)
+    
+  }
+  
+  library(x, character.only = TRUE)
+  
+}
+
+############################################
+# {sqldf}
+# Basic manipulation 
+############################################
+checkuse("sqldf")
