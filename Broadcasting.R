1fff09b09d6d1d2126671b7257d517d8a338dac3
Min Lu <luminwin@gmail.com>
2021-12-01 08:44:21 -0500

    20211201

--- Broadcasting.R
+++ Broadcasting.R
@@ -2,3 +2,23 @@
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
+
+###################################################################	
+### 
+### BST 625 Contributors' work
+###
+###################################################################
+checkuse <-function(x){
+  if(x %in% rownames(installed.packages()) == FALSE) {
+    install.packages(x)
+  }
+  # library(x, character.only = TRUE)
+}
+
+checkuse("maps")
+checkuse("tidyverse")
+checkuse("skimr")
+checkuse("data.table")
+checkuse("cowsay")
+dat <- read.csv("http://courses.washington.edu/b517/Datasets/shoulder.csv")
+Pain_long <- dat$pain
