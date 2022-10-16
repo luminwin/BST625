3325b0b686118bb20edc42fc5710a3e9672bf8ad
Min Lu <luminwin@gmail.com>
2022-10-15 22:12:15 -0400

    Lu

--- Broadcasting.R
+++ Broadcasting.R
@@ -1,6 +1,4 @@
 ###############################################################################################################
-# This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
+# This file broadcasts the R code for BST 625 from 9:00 am ~ 11:30 am From Oct 17th to Nov 28th.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
-
-x <- 5
