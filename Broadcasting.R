50b4ca21291de982e6db287a4168dd0ac517af79
Lu <m.lu6@umiami.edu>
2022-10-17 09:50:58 -0400

    Lu

--- Broadcasting.R
+++ Broadcasting.R
@@ -29,3 +29,16 @@ ls()  # print all the objects created by you
 #install.packages("metavcov")  # install a package
 #library("metavcov")           # load a package
 #require("metavcov")           # load a package
+
+
+
+age <- 5:10
+mean(age) # calculate the average/mean
+mean(x = age, trim = 0.2, na.rm = TRUE) # trimmed mean
+
+mean(c(22, 15, 90, 55))
+
+mean(age, 0.2, TRUE)  # the same as mean(x = age, trim = 0.2, na.rm = TRUE)
+mean(age, 0.2)        # the same as mean(x = age, trim = 0.2, na.rm = FALSE)
+
+
