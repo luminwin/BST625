8ceb7bee9d1c61c63f6b254643a0fc8dcc85fc1b
Lu <m.lu6@umiami.edu>
2022-10-17 09:25:48 -0400

    Lu

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,4 +3,15 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
-age <- c(10,32,5,9)
+age <- c(5, 6, 7, 8, 9, 10)          # initialize a vector
+height <- c(103, 108, 112, 122, 129, 140)
+
+
+
+plot(age, height) # make a plot
+
+
+pdf("example.pdf", width = 5, height = 5)
+plot(age, height)
+dev.off()
+
