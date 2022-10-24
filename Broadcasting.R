f58876ad9decdb0362b58c567217355b9a7e887a
Lu <m.lu6@umiami.edu>
2022-10-24 09:24:48 -0400

    Lu

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,9 +3,31 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
-age <- c(10, 12, 25, 50, 55, 80)
+age <- c(10, 80, 12, 
+         25, 50, 55)
+
 mean(x = age, trim = 0.2) 
 
 height <- c(120, 118, 162, 160, 169, 170)
 mean(height[which(age>20)])   # conditional mean: average body height for participants older                                                      #  than 20 years old
 height[order(age)]  
+
+############
+# matrix
+############
+
+cbind(age, height) # create a matrix by binding the columns
+rbind(age, height) # create a matrix by binding the rows
+# create a matrix by specifying each element
+mdat <- matrix(c(1,2,3, 11,12,13), nrow = 3, ncol = 2) 
+mdat
+
+mdat <- matrix(c(1,2,3, 11,12,13), nrow = 3, ncol = 2, 
+               dimnames = list(Subject = c("Geometry","Algebra", "Reading"),
+                               Edu = c("Elementary", "Graduate")
+               )) 
+mdat
+## attributes() tells you the names and other information within an object
+attributes(mdat) 
+## str() compactly displays the structure of an arbitrary R object
+str(mdat)
