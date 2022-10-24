67857cbfcbad80d02b3ebd701ddaf4d95e6e8106
Lu <m.lu6@umiami.edu>
2022-10-24 10:35:37 -0400

    lu

--- Broadcasting.R
+++ Broadcasting.R
@@ -239,3 +239,28 @@ obj <- lm(height~age)
 plot(obj)
 methods(plot)
 attributes(obj)
+
+plot(mtcars$mpg ~ mtcars$wt,         # y ~ x
+     xlim = c(0, 6),       # set x axis limits from 0 to 30
+     ylim = c(10, 40),     # set y axis limits from 10 to 40
+     main = "Miles per Gallon and Weight", # title for this plot
+     xlab = "Weight (1000 lbs)",           # label for x axis
+     ylab = "Miles/ gallon",               # label for y axis
+     col = "red",          # color for the symbols
+     pch = 19)             # shape for the symbols
+
+### Please manipulate these inputs and see the changes in the output figure
+## choose variables displacement (cu.in.) and weight 
+## find colors in the following link
+## https://www.datanovia.com/en/blog/awesome-list-of-657-r-color-names/
+
+plot(mtcars$mpg ~ mtcars$wt,
+     #  xlim = c(0, 6),
+     #  ylim = c(10, 40),
+     main = "Miles per Gallon and Weight",
+     xlab = "Weight (1000 lbs)",
+     ylab = "Miles/ gallon",
+     col = "red",
+     pch = 19)
+
+
