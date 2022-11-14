ff391c02457e79d6961bb3a2083c9b810d7bab2d
Lu <m.lu6@umiami.edu>
2022-11-14 09:11:39 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -2,3 +2,17 @@
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
+
+
+HEART1 <- read.csv("C:/Users/m.lu/Downloads/BST625/R3/HEART.csv") ## {base}
+
+if("tidyverse" %in% rownames(installed.packages()) == FALSE) {install.packages("tidyverse")}
+library(tidyverse)
+
+HEART2 <- read_csv("C:/Users/m.lu/Downloads/BST625/R3/HEART.csv") ## {tidyverse}
+
+
+### check how Base R (HEART1) and tidyverse (HEART2) display data differently
+HEART1
+
+HEART2
