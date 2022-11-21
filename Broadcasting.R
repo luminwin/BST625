d02a1ad8efec9aa88ef55c9f1c44a0172948aa00
Lu <m.lu6@umiami.edu>
2022-11-21 08:50:34 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,4 +3,5 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
-# If you submit
+# If you submit your HW6 by this Friday with the bonus part (Nov 25th, 11:59pm), 
+# I will grade your homework early so that you can resubmit to claim all your lost points. 
