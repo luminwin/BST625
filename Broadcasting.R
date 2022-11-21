85be381e55b4e12521525a86bd4182889a8fd117
Lu <m.lu6@umiami.edu>
2022-11-21 10:08:31 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -126,3 +126,20 @@ dat %>% mutate(
   # Default smoother is LOESS
   geom_smooth() +
   geom_smooth(method = "lm", colour = "red")
+
+
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = grade)) +
+  labs(x = "Grade", y="Count Number")
