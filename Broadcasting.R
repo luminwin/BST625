f84472d07c072ddecbdb6da32ff2d7cc3bcdd272
Lu <m.lu6@umiami.edu>
2022-11-21 10:12:17 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -160,3 +160,42 @@ dat %>% mutate(
   ggplot() +
   geom_count(mapping = aes(x = grade, y = gender)) +
   labs(x = "Grade", y="Female/Male")
+
+
+########################################### slide # 6
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  count(grade, gender) %>%  
+  ggplot(mapping = aes(x = grade, y = gender)) +
+  geom_tile(mapping = aes(fill = n))+
+  labs(x = "Grade", y="Female/Male")
+
+
+
+########################################### slide # 7
+dat %>% mutate(
+  Total_Score = rowSums(select(., score1:score3), na.rm = TRUE), 
+  Avg_Score = rowMeans(select(.,score1:score3), na.rm = TRUE), 
+  grade = case_when( 
+    Avg_Score < 60 ~ "F", 
+    (60 <= Avg_Score)&(Avg_Score < 70) ~ "D", 
+    (70 <= Avg_Score)&(Avg_Score < 80) ~ "C", 
+    (80 <= Avg_Score)&(Avg_Score < 90) ~ "B", 
+    Avg_Score >= 90 ~ "A"),
+  pass = case_when(
+    grade == "F"~"Fail", 
+    TRUE ~"Pass")) %>%
+  ggplot(aes(gender, Avg_Score, fill = factor(major))) +
+  geom_boxplot()           
+
+
