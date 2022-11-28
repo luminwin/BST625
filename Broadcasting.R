598809d072851ca50e50efad995efa2363d63607
Lu <m.lu6@umiami.edu>
2022-11-28 09:05:14 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -3,4 +3,6 @@
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
 
-# 
+install.packages("sqldf") ## after installation, add # in front to put it in comment 
+# install.packages("RSQLite") 
+library("sqldf")
