f06b85178178de115cdc22262ae1e7a68e3b2e3c
Min Lu <luminwin@gmail.com>
2022-11-28 00:45:10 -0500

    ss

--- Broadcasting.R
+++ Broadcasting.R
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
 #############################################################################################################
 # This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
 # After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
 ################################################################################################################
- 
-# 
-=======
-###############################################################################################################
-# This file broadcasts the R code for BST 625 from 9:00 am ~ 10:30 am From Oct 11th to Dec 1st.
-# After the class, this file will be empty. You can find all the R code files for BST 625  in its Blackboard.
-################################################################################################################
-
-# If you submit your HW6 by this Friday with the bonus part (Nov 25th, 11:59pm), 
-# I will grade your homework early so that you can resubmit to claim all your lost points. 
-
-# Please download "team.jpg" and "score_data.RData" from our blackboard to join class
->>>>>>> 1bd84d047d7cbb104e0abf01ed42f315317aed66
